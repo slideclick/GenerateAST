7d082f35d306d834f8f1de19332b8d39a428d699
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2017-08-17 11:38:22 +0800

    first check in

---
+++ GenerateAST/GenerateAST.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateAST
+{
+    class GenerateAST
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
